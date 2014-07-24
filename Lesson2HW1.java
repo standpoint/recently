7e75c3492cf319aee8be70a80d5cf0c53afbccc1
bubens <v.kamenskyi@gmail.com>
2014-07-24 23:16:00 +0300

    Update Lesson2HW1.java

--- Lesson2HW1.java
+++ Lesson2HW1.java
@@ -9,24 +9,50 @@
 public class Lesson2HW1 {
 	public static void main(String[] args) {
 		double a, b, c;
-		a = 15;
-		b = 140;
-		c = 16;
-		
+		a = 6;
+		b = 4;
+		c = 4;
+
 		// Check is triangle can exist with sides a,b,c at all
 		if (a < (b + c) && b < (a + c) && c < (a + b)) {
-			
+
 			// Check type of the triangle
 			if (a == b && b == c && a == c) {
 				System.out.println("The triangle is an equilateral");
 			}
 			else if (a == b || b == c || a == c) {
-				System.out.println("The triangle is an isosceles");
+				System.out.print("The triangle is an isosceles and ");
+				if ((Math.pow(a, 2) + Math.pow(b, 2)) > Math.pow(c, 2) && 
+					(Math.pow(b, 2) + Math.pow(c, 2)) > Math.pow(a, 2) &&
+					(Math.pow(a, 2) + Math.pow(c, 2)) > Math.pow(b, 2)) {
+					System.out.println("an acute-angle");
+				}
+				else if ((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2) || 
+						(Math.pow(b, 2) + Math.pow(c, 2)) == Math.pow(a, 2) ||
+						(Math.pow(a, 2) + Math.pow(c, 2)) == Math.pow(b, 2)) {
+					System.out.println("a right-angle");
+				}
+				else {
+					System.out.println("an obtuse");
+				}				
 			}
 			else {
-				System.out.println("The triangle is an scalene");
-			}
-		} 
+				System.out.print("The triangle is a scalene and ");
+				if ((Math.pow(a, 2) + Math.pow(b, 2)) > Math.pow(c, 2) && 
+					(Math.pow(b, 2) + Math.pow(c, 2)) > Math.pow(a, 2) &&
+					(Math.pow(a, 2) + Math.pow(c, 2)) > Math.pow(b, 2)) {
+					System.out.println("an acute-angle");
+				}
+				else if ((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2) || 
+						(Math.pow(b, 2) + Math.pow(c, 2)) == Math.pow(a, 2) ||
+						(Math.pow(a, 2) + Math.pow(c, 2)) == Math.pow(b, 2)) {
+					System.out.println("a right-angle");
+				}
+				else {
+					System.out.println("an obtuse");
+				}
+			}			
+		}		
 		else {
 			System.out.println("Any trianges with such sides (" + a + ", " + b + ", " + c + ") are not exist");
 		}
