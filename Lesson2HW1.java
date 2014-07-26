cec0f6c9a6ab275cee25ea220387c4fdc15f46be
bubens <v.kamenskyi@gmail.com>
2014-07-26 08:47:04 +0300

    Update Lesson2HW1.java
    a little changes ...

--- Lesson2HW1.java
+++ Lesson2HW1.java
@@ -8,15 +8,13 @@
  */
 public class Lesson2HW1 {
 
-	//Method 'angleCheck' check is the triangle acute-angled, right-angle or obtuse
-	public static String angleCheck(double... sides) {
-		double a = sides[0];
-		double b = sides[1];
-		double c = sides[2];
+	//Method 'checkAngle' check is the triangle acute-angled, right-angle or obtuse
+	public static void checkAngle(double a, double b, double c) {
+
 		if ((Math.pow(a, 2) + Math.pow(b, 2)) > Math.pow(c, 2) && 
 				(Math.pow(b, 2) + Math.pow(c, 2)) > Math.pow(a, 2) &&
 				(Math.pow(a, 2) + Math.pow(c, 2)) > Math.pow(b, 2)) {
-				System.out.println(" и остроугольным");
+			System.out.println(" и остроугольным");
 		}
 		else if ((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2) || 
 				(Math.pow(b, 2) + Math.pow(c, 2)) == Math.pow(a, 2) ||
@@ -26,11 +24,10 @@ public class Lesson2HW1 {
 		else {
 			System.out.println(" и тупоугольным");
 		}
-		return "";
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		// Coordinates of three points A(x1,y1), B(x2,y2), C(x3,y3) and segments a,b,c 
 		int x1, y1, x2, y2, x3, y3;
 		double a, b, c;
@@ -40,7 +37,7 @@ public class Lesson2HW1 {
 		y2 = -20;
 		x3 = 100;
 		y3 = 0;
-		
+
 		// Define sides of expected triangle
 		a = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
 		b = Math.sqrt(Math.pow((x3 - x2),2) + Math.pow((y3 - y2),2));
@@ -55,11 +52,11 @@ public class Lesson2HW1 {
 			}
 			else if (a == b || b == c || a == c) {
 				System.out.print("Треугольник с вершинами A(" + x1 + ", " + y1 + "), B(" + x2 + ", " + y2 + "), C(" + x3 + ", " + y3 + ") является равнобедренным");
-				angleCheck(a,b,c);
+				checkAngle(a,b,c);
 			}
 			else {
 				System.out.print("Треугольник с вершинами A(" + x1 + ", " + y1 + "), B(" + x2 + ", " + y2 + "), C(" + x3 + ", " + y3 + ") является разносторонним");
-				angleCheck(a,b,c);
+				checkAngle(a,b,c);
 			}
 		} 
 		else {
