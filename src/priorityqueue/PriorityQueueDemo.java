b2c0eff979920338c4275ec60116afaf8e3bf52f
standpoint <v.kamenskyi@gmail.com>
2014-11-03 11:33:16 +0200

    Priority Queue based on Heap Sorting algo

--- src/priorityqueue/PriorityQueueDemo.java
+++ src/priorityqueue/PriorityQueueDemo.java
@@ -5,6 +5,7 @@
 package priorityqueue;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -38,10 +39,63 @@ class Entry<Key,Value> {
     }
 }
 
-class PriorityQueue implements Iterable<Object>{
+class PriorityQueue implements Iterable<Entry>, Comparator<Entry> {
     
-
-    List<Entry> queue = new ArrayList<>();
+    Entry<Integer,String> node, left, right;
+    List<Entry> queue;
+    
+    public PriorityQueue() {
+        queue = new ArrayList();
+    }
+    
+    public PriorityQueue(ArrayList<Entry> list) {
+        queue = heapSort(list);
+    }
+    
+    private ArrayList<Entry> heapSort(ArrayList<Entry> list) {
+        buildHeap(list);
+        Iterator it = list.listIterator(list.size());
+        Entry task;
+        for (int i = list.size(); i >= 0 ; i -= 2) {
+            task = list.get(i);
+            swapNodes(list.get(0),task);
+        }
+        return list;
+    }
+    
+    private void swapNodes(Entry x1, Entry x2){
+        Entry temp;
+        temp = new Entry(x1.getPriority(),x1.getValue());
+        x1.setPriority(x2.getPriority());
+        x1.setValue(x2.getValue());
+        x2.setPriority(temp.getPriority());
+        x2.setPriority(temp.getPriority());
+    }
+    
+    private void buildHeap(ArrayList<Entry> list) {
+        for(int i = list.size(); i >=0; i -= 2) {
+            heapify(list,i);
+        }
+    }
+    
+    private void heapify(ArrayList<Entry> list, int i) {
+        int l = i*2+1;
+        int r = i*2+2;
+        int size = list.size();
+        int lagest = size - 1;
+        left = list.get(l);
+        right = list.get(r);
+        if(l < size && compare(left,list.get(i)) >= 0) {
+            lagest = l;
+        } else lagest = i;
+        if (r < size && compare(right,list.get(i)) >= 0) {
+            lagest = r;
+        }
+        if (lagest != i) {
+            swapNodes(list.get(i), list.get(lagest));
+            heapify(list, lagest);
+        }
+    }
     
     public Entry peak() {
         if (queue.isEmpty()) {
@@ -54,9 +108,8 @@ class PriorityQueue implements Iterable<Object>{
     public Entry poll() {
         if (queue.isEmpty()) {
             return new Entry(0,"queue is empty");
-        } else {
-            return queue.remove(0);
         }
+        return queue.remove(0);
     }
     
     public boolean add(Entry item) {
@@ -91,6 +144,11 @@ class PriorityQueue implements Iterable<Object>{
     public Iterator iterator() {
         return queue.iterator();
     }
+
+    @Override
+    public int compare(Entry t, Entry t1) {
+        return t.getPriority() - t1.getPriority();
+    }
 }
 
 public class PriorityQueueDemo {
@@ -99,24 +157,41 @@ public class PriorityQueueDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int priority = 0;
-        Entry[] entries = new Entry[5];
-        PriorityQueue priorityQueue = new PriorityQueue();
-        for (Entry entry : entries) {
+//        int priority = 0;
+//        Entry[] entries = new Entry[5];
+//        PriorityQueue priorityQueue = new PriorityQueue();
+//        for (Entry entry : entries) {
+//            priority = (int)(Math.random()*100);
+//            entry = new Entry(priority,("The entry [" + priority + "]"));
+//            priorityQueue.add(entry);
+//            
+//        }
+//        priorityQueue.add(new Entry(100,"The entry with highest priority"));
+//        priorityQueue.add(new Entry(0,"The entry with lowest priority"));
+//        System.out.println(priorityQueue.toString());
+//        System.out.println("peak: " + priorityQueue.peak().toString());
+//        Iterator it = priorityQueue.iterator();
+//        while (it.hasNext()) {
+//            System.out.println("poll: " + priorityQueue.poll().toString());
+//        }
+//        System.out.println("after polling: " + priorityQueue.toString());
+        Integer priority;
+        String value;
+        List<Entry> list = new ArrayList<>();
+        Entry[] tasks = new Entry[5];
+        for (Entry task : tasks) {
             priority = (int)(Math.random()*100);
-            entry = new Entry(priority,("The entry [" + priority + "]"));
-            priorityQueue.add(entry);
+            value = "The task [" + priority + "]";
+            task = new Entry(priority,value);
+            list.add(task);
+            task.toString();
+        }
+        Iterator<Entry> it = list.iterator();
+        while(it.hasNext()) {
+            Entry task = it.next();
+            task.toString();
             
         }
-        priorityQueue.add(new Entry(100,"The entry with highest priority"));
-        priorityQueue.add(new Entry(0,"The entry with lowest priority"));
-        System.out.println(priorityQueue.toString());
-        System.out.println("peak: " + priorityQueue.peak().toString());
-        Iterator it = priorityQueue.iterator();
-        while (it.hasNext()) {
-            System.out.println("poll: " + priorityQueue.poll().toString());
-        }
-        System.out.println("after polling: " + priorityQueue.toString());
                
     }
     
